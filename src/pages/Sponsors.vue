67731edce289943409374022be4511ce693cdf07
Mulvey <dlmf1ccd@thompsonschools.org>
2019-02-12 15:54:57 -0700

    More work

---
+++ src/pages/Sponsors.vue
@@ -0,0 +1,115 @@
+<template>
+    <div class="content">
+        <a href="https://nasa.gov/" target="_blank">
+            <div class="infocard link">
+                <img src="src/assets/images/nasa-logo.png" class="logo" style="width: 10rem; height: 10rem;">
+                <div class="linktext">
+                    <h2>NASA</h2>
+                    <p>2019 - Sustainability Grant - $5000</p>
+                    <p>https://nasa.gov/</p>
+                </div>
+            </div>
+        </a>
+
+        <a href="http://www.northropgrumman.com" target="_blank">
+            <div class="infocard link">
+                <img src="src/assets/images/grumman-logo.svg" class="logo" style="width: 10rem; height: 10rem;">
+                <div class="linktext">
+                    <h2>Northrup Gruman</h2>
+                    
+                    <p>2017 - $1600</p>
+                    <p>2018 - $2400</p>
+                    <p>2019 - $1400</p>
+                    <p>http://www.northropgrumman.com/</p>
+                </div>
+            </div>
+        </a>
+
+        <a href="https://www.ottercares.org/" target="_blank">
+            <div class="infocard link">
+                <img src="src/assets/images/ottercares-logo.svg" class="ottercares">
+                <div class="linktext">
+                    <h2>Ottercares</h2>
+                    <p>2016 - Impact Grant - $18600 </p>
+                    <p>2018 - Innovation Station - $25000</p>
+                    <p>https://www.ottercares.org/</p>
+                </div>
+            </div>
+        </a>
+
+        <a href="https://www.jbcsllc.com/" target="_blank">
+            <div class="infocard link">
+                <div class="linktext">
+                    <h2>Beatty Construction</h2>
+                    <p>2019 - $1000</p>
+                    <p>https://www.jbcsllc.com/</p>
+                </div>
+            </div>
+        </a>
+
+        <a href="https://www.usengineering.com/">
+            <div class="infocard link">
+                <div class="linktext">
+                    <img src="src/assets/images/us-logo.svg" style="width: 10rem; height: 10rem;" class="logo">
+                    <h2>US Engineering</h2>
+                    <p>2017 - $500</p>
+                    <p>2018 - $500</p>
+                </div>
+            </div>
+        </a>
+
+        <a href="https://www.ptc.com/en/" target="_blank">
+            <div class="infocard link">
+                <img src="src/assets/images/ptc-logo.svg" class="logo">
+                <div class="linktext">
+                    <h2>PTC</h2>
+                    <p>2017 - $2000</p>
+                    <p>https://www.ptc.com/</p>
+                </div>
+            </div>
+        </a>
+
+        <a href="https://www.lowes.com/" target="_blank">
+            <div class="infocard link">
+                <img src="src/assets/images/lowes-logo.svg" class="logo">
+                <div class="linktext">
+                    <h2>Lowe's</h2>
+                    <p>2016 - Toolbox for Educators - $5000</p>
+                    <p>https://www.lowes.com/</p>
+                </div>
+            </div>
+        </a>
+
+        <a href="https://www.lockheedmartin.com/en-us/index.html" target="_blank">
+            <div class="infocard link">
+                <img src="src/assets/images/lockheed-logo.svg" style="width: 14rem; height: 14rem;" class="logo">
+                <div class="linktext">
+                    <h2>Lockheed Martin</h2>
+                    <p>2016 - $1600</p>
+                    <p>https://www.lockheedmartin.com/</p>
+                </div>
+            </div>
+        </a>
+
+        <!--<a href="" target="_blank">
+            <div class="infocard link">
+                <img src="src/assets/images/-logo.svg" class="logo">
+                <div class="linktext">
+                    <h2>Sponsor</h2>
+                    <p>description</p>
+                    <p>link</p>
+                </div>
+            </div>
+        </a>-->
+    </div>
+</template>
+
+<script>
+export default {
+    name: "contact"
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,1 +1,33 @@
-// bleh
+import Songs from "@/pages/Library/Songs";
+import Vue from "vue";
+import Router from "vue-router";
+Vue.use(Router);
+
+export default new Router({
+    routes: [
+        {
+            path: "*",
+            redirect: "/Home"
+        },
+        {
+            path: "/Home",
+            name: "Home",
+            component: require("@/pages/Home").default
+        },
+        {
+            path: "/Gallery",
+            name: "Gallery",
+            component: require("@/pages/Gallery").default
+        },
+        {
+            path: "/Contact",
+            name: "Contact",
+            component: require("@/pages/Contact").default
+        },
+        {
+            path: "/Links",
+            name: "Links",
+            component: require("@/pages/Links").default
+        },
+    ]
+});
